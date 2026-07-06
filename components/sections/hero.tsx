31bbb19f5b2de5742300a7cd14d3189ca2bdbfae
lucasdef15 <lucasff15@hotmail.com>
2026-07-06 19:06:54 -0300

    chore: initialize Next.js project

---
+++ components/sections/hero.tsx
@@ -0,0 +1,11 @@
+import Container from "../ui/container";
+
+export default function Hero() {
+  return (
+    <section className="relative overflow-hidden py-32">
+      <Container>
+        <p>This is hero</p>
+      </Container>
+    </section>
+  );
+}
---
+++ components/ui/container.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+export default function Container({ children }: ContainerProps) {
+  return <div className="mx-auto w-full max-w-7xl px-6 lg:px-8">{children}</div>;
+}
